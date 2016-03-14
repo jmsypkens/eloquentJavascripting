0aeb831725feb618ae0c111cdd459a16bd8cf90c
Jerry Sypkens <lovehalfblack@gmail.com>
2016-03-13 22:00:41 -0700

    looping triangle

---
+++ chap2/loopingTriangle.js
@@ -0,0 +1,6 @@
+// This program loops a # 14 times to create a triangle 
+
+for (var hashTaggy = "#"; hashTaggy.length <= 14; hashTaggy = hashTaggy + "#") 
+    console.log(hashTaggy);
+
+// jerrysypkens.com
