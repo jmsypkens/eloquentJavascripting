9b4418f13639cbfa40792e95c39255ad3cc81ce0
Jerry Sypkens <lovehalfblack@gmail.com>
2016-03-13 23:20:56 -0700

    min number function

---
+++ chap3/min.js
@@ -0,0 +1,13 @@
+// jerrysypkens.com 
+//Write a function min that takes two arguments and returns their minimum.
+
+var min = function(a, b) {
+    if (a > b)
+        return b;
+    else 
+        return a;
+}
+
+console.log(min(2, 10));
+
+// console logs = 2 
